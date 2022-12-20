5528c07232d12349b48b626eaa8805095d23ccc4
nayeem202 <nayeem.ahmedemba@gmail.com>
2022-12-20 17:08:13 +0600

    swagger3

--- src/main/java/devDojoAcademy/WebFluxEssentials/WebFluxEssentialsApplication.java
+++ src/main/java/devDojoAcademy/WebFluxEssentials/WebFluxEssentialsApplication.java
@@ -1,15 +1,11 @@
 package devDojoAcademy.WebFluxEssentials;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import reactor.blockhound.BlockHound;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 @SpringBootApplication
-@EnableSwagger2
 public class WebFluxEssentialsApplication {
 
 /*	static {
@@ -24,5 +20,6 @@ public class WebFluxEssentialsApplication {
 		SpringApplication.run(WebFluxEssentialsApplication.class, args);
 
 	}
+
 
 }
